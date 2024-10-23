704b2cad48d0da8e64bbf8474707503a0f9e778c
frenesix <bernardlereceveur@gmail.com>
2024-10-24 00:12:02 +0200

    "Updated routes in web.php to use HomeController and LoginController, added new routes for home, about, and dashboard, and commented out login and register routes."

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'home'])->name('app_home');
+Route::get('/about',[HomeController::class,'about'])->name('app_about');
+
+Route::match(['get','post'],'/dashboard', [HomeController::class , 'dashboard'])->name(('app_dashboard'));
+/*Route::match(['get','post'],'/login', [LoginController::class , 'login'])->name(('app_login'));
+Route::match(['get','post'],'/register', [LoginController::class , 'register'])->name(('app_register'));*/
