9cb31dcf06d14bdc65b143cc7823d4ce1ac01bab
frenesix <bernardlereceveur@gmail.com>
2024-10-25 15:55:08 +0200

    "Remove two-factor authentication migration and update register.blade.php"

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,8 @@
             </div>
             <div class="col-md-6 mx-auto mt-3">
                 <label for="inputEmail4" class="form-label">Email</label>
-                <input type="email" class="form-control" id="inputEmail4" name="email" required autocomplete="email" url-existEmail="{{route('app_existEmail'}}"token="{{csrf_token()}}">
+                <input type="email" class="form-control" id="inputEmail4" name="email" required autocomplete="email" data-url-exist-email="{{ route('app_existEmail') }}" data-token="{{ csrf_token() }}">
+
             </div>
             <div class="col-md-6 mx-auto mt-3">
                 <label for="inputPassword4" class="form-label">Mot de passe</label>
